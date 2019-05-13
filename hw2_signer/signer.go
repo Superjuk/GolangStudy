86462fad35a43cf10175554aa14f1d6c7ca06ab5
Superjuk <superjuk@gmail.com>
2019-05-13 15:58:21 +0300

    Change model

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -10,38 +10,30 @@ import (
 )
 
 var (
-	dummy        = make(chan interface{})
+	wgGl         = &sync.WaitGroup{}
 	dataArr      []string
 	dataArrMutex = &sync.Mutex{}
-	wgGl         = &sync.WaitGroup{}
 	md5Mutex     = &sync.Mutex{}
+	testMutex    = &sync.Mutex{}
 	numsStrArr   = [...]string{"0", "1", "2", "3", "4", "5"}
 )
 
 func ExecutePipeline(jobs ...job) {
-	rawDataCh := make(chan interface{}, MaxInputDataLen)
+	var chans []chan interface{}
+	dummy := make(chan interface{})
 
-	wgGl.Add(1)
-	go jobWrapper(jobs[0], dummy, rawDataCh)
+	chans = append(chans, dummy)
 
-	jobsSlice := jobs[1:]
-
-	for num := range rawDataCh {
-		var chans []chan interface{}
-		for range jobsSlice {
-			wgGl.Add(1)
-			chans = append(chans, make(chan interface{}, MaxInputDataLen))
-		}
-		chans = append(chans, dummy)
-
-		chans[0] <- num
-
-		for i, job := range jobsSlice {
-			go jobWrapper(job, chans[i], chans[i+1])
-		}
-
-		close(chans[0])
+	for range jobs {
+		wgGl.Add(1)
+		chans = append(chans, make(chan interface{}, MaxInputDataLen))
 	}
+
+	for i, job := range jobs {
+		go jobWrapper(job, chans[i], chans[i+1])
+	}
+
+	close(chans[0])
 
 	wgGl.Wait()
 }
@@ -49,9 +41,7 @@ func ExecutePipeline(jobs ...job) {
 /**/
 func jobWrapper(jb job, in, out chan interface{}) {
 	defer wgGl.Done()
-	if out != dummy {
-		defer close(out)
-	}
+	defer close(out)
 
 	jb(in, out)
 
@@ -60,96 +50,104 @@ func jobWrapper(jb job, in, out chan interface{}) {
 
 /*SingleHash count MD5 and CRC32*/
 func SingleHash(in, out chan interface{}) {
-LOOP:
-	for {
-		select {
-		case data := <-in:
-			if data == nil {
-				out <- data
-				break LOOP
-			}
-
-			dataStr := strconv.Itoa(data.(int))
-
-			wg := &sync.WaitGroup{}
-			wg.Add(2)
-
-			dataSl := make([]string, 2)
-
-			md5 := func(str string) *string {
-				md5Mutex.Lock()
-				md5Out := DataSignerMd5(str)
-				md5Mutex.Unlock()
-				return &md5Out
-			}
-
-			go Crc32Worker(wg, md5(dataStr), &dataSl[1])
-			go Crc32Worker(wg, &dataStr, &dataSl[0])
-
-			wg.Wait()
-
-			out <- strings.Join(dataSl, "~")
-
-			runtime.Gosched()
-		}
+	md5 := func(str string) string {
+		md5Mutex.Lock()
+		md5Out := DataSignerMd5(str)
+		md5Mutex.Unlock()
+		return md5Out
 	}
+
+	hash := func(str string, out chan interface{}, wgSH *sync.WaitGroup) {
+		wg := &sync.WaitGroup{}
+		wg.Add(2)
+
+		dataSl := make([]string, 2)
+
+		go Crc32Worker(wg, str, dataSl[0:1])
+		go Crc32Worker(wg, md5(str), dataSl[1:2])
+
+		wg.Wait()
+
+		fmt.Println("SingleHash out =", strings.Join(dataSl, "~"))
+		out <- strings.Join(dataSl, "~")
+
+		wgSH.Done()
+
+		runtime.Gosched()
+	}
+
+	wgSH := &sync.WaitGroup{}
+
+	for data := range in {
+		wgSH.Add(1)
+		dataStr := strconv.Itoa(data.(int))
+
+		go hash(dataStr, out, wgSH)
+	}
+
+	wgSH.Wait()
+
+	fmt.Println("Single hash done!!!")
 }
 
 func MultiHash(in, out chan interface{}) {
-LOOP:
-	for {
-		select {
-		case data := <-in:
-			if data == nil {
-				out <- data
-				break LOOP
-			}
+	hash := func(str string, out chan interface{}, wgMh *sync.WaitGroup) {
+		wg := &sync.WaitGroup{}
+		wg.Add(6)
 
-			wg := &sync.WaitGroup{}
-			wg.Add(6)
+		dataSl := make([]string, 6)
 
-			dataSl := make([]string, 6)
-
-			for i, num := range numsStrArr {
-				line := num + data.(string)
-				go Crc32Worker(wg, &line, &dataSl[i])
-			}
-
-			wg.Wait()
-
-			out <- strings.Join(dataSl, "")
-
-			runtime.Gosched()
+		for i, num := range numsStrArr {
+			line := num + str
+			go Crc32Worker(wg, line, dataSl[i:i+1])
 		}
+
+		wg.Wait()
+
+		fmt.Println("MultiHash out =", strings.Join(dataSl, ""))
+		out <- strings.Join(dataSl, "")
+
+		wgMh.Done()
+
+		runtime.Gosched()
 	}
+
+	wgMh := &sync.WaitGroup{}
+
+	for data := range in {
+		wgMh.Add(1)
+
+		go hash(data.(string), out, wgMh)
+	}
+
+	wgMh.Wait()
+
+	fmt.Println("MultiHash done!!!")
 }
 
 func CombineResults(in, out chan interface{}) {
-LOOP:
 	for data := range in {
-		fmt.Println("Combine results fires!")
-		fmt.Println("DataArr len=", len(dataArr))
-		if data == nil {
-			fmt.Println("data nil!")
-			dataArrMutex.Lock()
-			sort.Strings(dataArr)
-			result := strings.Join(dataArr, "_")
-			dataArrMutex.Unlock()
-			out <- result
-			break LOOP
-		}
-
 		dataArrMutex.Lock()
 		dataArr = append(dataArr, data.(string))
+		fmt.Println("dataArr =", dataArr)
 		dataArrMutex.Unlock()
 
 		runtime.Gosched()
 	}
+
+	fmt.Println("Combine results fires!")
+	fmt.Println("DataArr len=", len(dataArr))
+
+	sort.Strings(dataArr)
+	result := strings.Join(dataArr, "_")
+	fmt.Println("result =", result)
+
+	out <- result
 }
 
-func Crc32Worker(wg *sync.WaitGroup, data *string, out *string) {
-	crc32 := DataSignerCrc32(*data)
-	*out = crc32
+func Crc32Worker(wg *sync.WaitGroup, data string, slice []string) {
+	crc32 := DataSignerCrc32(data)
+	slice[0] = crc32
 	wg.Done()
 	runtime.Gosched()
 }

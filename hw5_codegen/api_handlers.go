fcb5548765f5eb2a54af02d7802080267e5fd56f
Superjuk <superjuk@gmail.com>
2019-09-09 19:34:36 +0300

    Realizing validating functions. Need refactoring.

--- hw5_codegen/api_handlers.go
+++ hw5_codegen/api_handlers.go
@@ -11,12 +11,14 @@ import (
 	"strconv"
 )
 
+/*out must be slice of strings or struct of strings*/
 func fillProfileParams(query url.Values) (out *ProfileParams) {
 	out = &ProfileParams{}
 	out.Login = query.Get("login")
 	return out
 }
 
+/*in must be slice of strings or struct of strings*/
 func validateProfileParams(in *ProfileParams) (out *ProfileParams, err error) {
 	out = in
 	err = nil
@@ -27,8 +29,23 @@ func validateProfileParams(in *ProfileParams) (out *ProfileParams, err error) {
 	return out, err
 }
 
-func validateCreateParams() {
+/*out must be slice of strings or struct of strings*/
+func fillCreateParams(query url.Values) (out *CreateParams) {
+	out = &CreateParams{}
+	out.Login = query.Get("login")
+	out.Name = query.Get("full_name")
+	out.Status = query.Get("status")
+	out.Age, _ = strconv.Atoi(query.Get("age"))
+	return out
+}
 
+/*in must be slice of strings or struct of strings*/
+func validateCreateParams(in *CreateParams) (out *CreateParams, err error) {
+	//required
+	if in.Login == "" {
+		err = errors.New("login must not me empty")
+	}
+	return out, err
 }
 
 func (h *MyApi) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -73,7 +90,28 @@ func (h *MyApi) handlerProfile(w http.ResponseWriter, r *http.Request) {
 
 func (h *MyApi) handlerCreate(w http.ResponseWriter, r *http.Request) {
 	// заполнение структуры params
+	raw := fillCreateParams(r.URL.Query())
 	// валидирование параметров
-	//res, err := h.Create(ctx, params)
+	params, err := validateCreateParams(raw)
+	if err != nil {
+		/* исправить вывод в соответствии с main_test.go */
+		w.Write([]byte(err.Error()))
+		return
+	}
+	ctx := context.Background()
+	res, err := h.Create(ctx, *params)
+	if err != nil {
+		/* исправить вывод в соответствии с main_test.go */
+		w.Write([]byte(err.Error()))
+		return
+	}
 	// прочие обработки
+	/* вывод должен быть в json */
+	result := `{"error": "", "response": {
+		"id": ` + strconv.FormatUint(res.ID, 10) + `,
+		"login": "` + res.Login + `",
+		"full_name": "` + res.FullName + `",
+		"status": ` + strconv.Itoa(res.Status) + `}}`
+
+	w.Write([]byte(result))
 }

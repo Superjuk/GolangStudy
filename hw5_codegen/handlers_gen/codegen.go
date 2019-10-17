4255c710c7dcdf54bce82c174d8646a0840d2419
Superjuk <superjuk@gmail.com>
2019-10-17 17:55:13 +0300

    -

--- hw5_codegen/handlers_gen/codegen.go
+++ hw5_codegen/handlers_gen/codegen.go
@@ -19,20 +19,21 @@ func main() {
 		log.Fatal("ParseFile error: ", err.Error())
 	}
 
-	out, _ := os.Create(os.Args[2])
+	//out, _ := os.Create(os.Args[2])
 
-	fmt.Fprintln(out, `package `+apiFile.Name.Name)
-	fmt.Fprintln(out)
-	fmt.Fprintln(out, `import "context"`)
-	fmt.Fprintln(out, `import "encoding/json"`)
-	fmt.Fprintln(out, `import "fmt"`)
-	fmt.Fprintln(out, `import "log"`)
-	fmt.Fprintln(out, `import "net/http"`)
-	fmt.Fprintln(out, `import "net/url"`)
-	fmt.Fprintln(out, `import "strconv"`)
-	fmt.Fprintln(out)
+	// fmt.Fprintln(out, `package `+apiFile.Name.Name)
+	// fmt.Fprintln(out)
+	// fmt.Fprintln(out, `import "context"`)
+	// fmt.Fprintln(out, `import "encoding/json"`)
+	// fmt.Fprintln(out, `import "fmt"`)
+	// fmt.Fprintln(out, `import "log"`)
+	// fmt.Fprintln(out, `import "net/http"`)
+	// fmt.Fprintln(out, `import "net/url"`)
+	// fmt.Fprintln(out, `import "strconv"`)
+	// fmt.Fprintln(out)
 
 	// it work's
+	fmt.Println("Declarations:")
 	for _, decl := range apiFile.Decls {
 		pos := decl.Pos()
 		relPosition := fset.Position(pos)

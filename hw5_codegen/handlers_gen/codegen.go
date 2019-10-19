230fefa6fefe2469fce07c52fd26aff6e344925b
Anton Semenov <superjuk@gmail.com>
2019-10-19 21:37:45 +0300

    Retrieve all needed information for code generation

--- hw5_codegen/handlers_gen/codegen.go
+++ hw5_codegen/handlers_gen/codegen.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"os"
-	// "go/ast"
+	"go/ast"
 	"go/parser"
 	"go/token"
 	"log"
+	"os"
 
 	//"io/ioutil"
 	"fmt"
@@ -56,36 +56,90 @@ const (
 func main() {
 	fset := token.NewFileSet()
 
-	apiFile, err := parser.ParseFile(fset, os.Args[1], nil, parser.ParseComments)
+	apigen, err := parser.ParseFile(fset, os.Args[1], nil, parser.ParseComments)
 	if err != nil {
 		log.Fatal("ParseFile error: ", err.Error())
 	}
 
-	out, _ := os.Create(os.Args[2])
+	// out, _ := os.Create(os.Args[2])
 
-	fmt.Fprintln(out, `package `+apiFile.Name.Name)
-	fmt.Fprintln(out)
-	fmt.Fprintln(out, `import "context"`)
-	fmt.Fprintln(out, `import "encoding/json"`)
-	fmt.Fprintln(out, `import "fmt"`)
-	fmt.Fprintln(out, `import "log"`)
-	fmt.Fprintln(out, `import "net/http"`)
-	fmt.Fprintln(out, `import "net/url"`)
-	fmt.Fprintln(out, `import "strconv"`)
-	fmt.Fprintln(out)
-	fmt.Fprintln(out, responseErr)
-	fmt.Fprintln(out)
-	fmt.Fprintln(out, responseOk)
-	fmt.Fprintln(out)
-	fmt.Fprintln(out, sendResponse)
+	// fmt.Fprintln(out, `package `+apiFile.Name.Name)
+	// fmt.Fprintln(out)
+	// fmt.Fprintln(out, `import "context"`)
+	// fmt.Fprintln(out, `import "encoding/json"`)
+	// fmt.Fprintln(out, `import "fmt"`)
+	// fmt.Fprintln(out, `import "log"`)
+	// fmt.Fprintln(out, `import "net/http"`)
+	// fmt.Fprintln(out, `import "net/url"`)
+	// fmt.Fprintln(out, `import "strconv"`)
+	// fmt.Fprintln(out)
+	// fmt.Fprintln(out, responseErr)
+	// fmt.Fprintln(out)
+	// fmt.Fprintln(out, responseOk)
+	// fmt.Fprintln(out)
+	// fmt.Fprintln(out, sendResponse)
 
 	// it work's
 	fmt.Println("Declarations:")
-	for _, decl := range apiFile.Decls {
-		pos := decl.Pos()
-		relPosition := fset.Position(pos)
-		log.Println(relPosition.String())
+	for _, decl := range apigen.Decls {
+		// анализируем функции
+		/*if gen, ok := decl.(*ast.FuncDecl); ok {
+			if gen.Doc.Text() != "" {
+				fmt.Println("Name:", gen.Name.String())
+				fmt.Println("Doc:", gen.Doc.Text())
+				//fmt.Println("Func:", gen.Type.Func)
+				if gen.Recv.NumFields() > 0 {
+					fmt.Println("Recv:", gen.Recv.List[0].Type)
+				}
+			}
+
+			fmt.Println("")
+		}*/
+
+		// анализируем структуры
+		if gen, ok := decl.(*ast.GenDecl); ok {
+			for _, spec := range gen.Specs {
+				specType, ok := spec.(*ast.TypeSpec)
+				if !ok {
+					continue
+				}
+				fmt.Println("specType:", specType.Name.Name)
+				structType, ok := specType.Type.(*ast.StructType)
+				if !ok {
+					continue
+				}
+				for _, field := range structType.Fields.List {
+					for _, name := range field.Names {
+						fmt.Println(name)
+					}
+					// if field.Comment != nil {
+					// 	fmt.Println(field.Comment.Text())
+					// }
+					// if field.Doc != nil {
+					// 	fmt.Println(field.Doc.Text())
+					// }
+					if field.Tag != nil {
+						//fmt.Println(field.Tag.Kind.String())
+						fmt.Println(field.Tag.Value)
+					}
+
+					fieldType, ok := field.Type.(*ast.Ident)
+					if ok {
+						fmt.Println(fieldType)
+					}
+				}
+				fmt.Println("@@")
+			}
+
+			fmt.Println("--")
+		}
 	}
+
+	// all comments
+	/*cmap := ast.NewCommentMap(fset, apigen, apigen.Comments)
+	fmt.Println(cmap)*/
+
+	// filtering
 
 	/*API realization*/
 

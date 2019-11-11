9dc1250da9d63d6a26e7d377d8f5c9c5c4440e3a
Superjuk <superjuk@gmail.com>
2019-11-11 18:49:58 +0300

    ServeHTTP method

--- hw6_db_explorer/db_explorer.go
+++ hw6_db_explorer/db_explorer.go
@@ -1,4 +1,31 @@
 package main
 
-// тут вы пишете код
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"strings"
+	"sync"
+)
+
 // обращаю ваше внимание - в этом задании запрещены глобальные переменные
+type DbApi struct {
+	mu sync.Mutex
+}
+
+func NewDbExplorer(db *sql.DB) (*DbApi, error) {
+	return &DbApi{}, nil
+}
+
+func (db *DbApi) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		fmt.Println("It works!!!")
+	default:
+		command := strings.Split(r.URL.Path, "/")
+		fmt.Println(command)
+		if len(command) <= 3 {
+			fmt.Println(r.URL.Query())
+		}
+	}
+}
